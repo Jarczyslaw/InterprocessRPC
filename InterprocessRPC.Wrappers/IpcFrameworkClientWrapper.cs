db80fbe844ae73543bc9ae2b2d47a0aa6f356fd0
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-10-28 19:17:27 +0100

    dev

---
+++ InterprocessRPC.Wrappers/IpcFrameworkClientWrapper.cs
@@ -0,0 +1,40 @@
+﻿using InterprocessRPC.Common;
+using JKang.IpcServiceFramework;
+using System;
+using System.Threading.Tasks;
+
+namespace InterprocessRPC.Wrappers
+{
+    public class IpcFrameworkClientWrapper : IClientWrapper
+    {
+        private IpcServiceClient<IProxy> client;
+
+        public Task<bool> CheckConnection()
+        {
+            return client.InvokeAsync(p => p.CheckConnection());
+        }
+
+        public Task<string> GetHelloMessage(string name)
+        {
+            return client.InvokeAsync(p => p.GetHelloMessage(name));
+        }
+
+        public Task<DateTime> GetServerTime()
+        {
+            return client.InvokeAsync(p => p.GetServerTime());
+        }
+
+        public Task Start()
+        {
+            client = new IpcServiceClientBuilder<IProxy>()
+                .UseNamedPipe("ipcFrameworkPipeName")
+                .Build();
+            return Task.CompletedTask;
+        }
+
+        public Task Stop()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ InterprocessRPC.Wrappers/IpcFrameworkServerWrapper.cs
@@ -0,0 +1,45 @@
+﻿using InterprocessRPC.Common;
+using JKang.IpcServiceFramework;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace InterprocessRPC.Wrappers
+{
+    public class IpcFrameworkServerWrapper : IServerWrapper
+    {
+        public Action ListeningStart { get; set; }
+        public Action ListeningStop { get; set; }
+        public Action ClientConnected { get; set; }
+        public Action ClientDisconnected { get; set; }
+
+        private readonly IServiceCollection services;
+
+        public IpcFrameworkServerWrapper()
+        {
+            services = ConfigureServices(new ServiceCollection());
+        }
+
+        public async Task Start()
+        {
+            var host = new IpcServiceHostBuilder(services.BuildServiceProvider())
+                .AddNamedPipeEndpoint<IProxy>("ipcFrameworkEndpoint", "ipcFrameworkPipeName")
+                .Build();
+            await host.RunAsync();
+        }
+
+        public Task Stop()
+        {
+            return Task.CompletedTask;
+        }
+
+        private static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            return services.AddIpc(builder =>
+            {
+                builder.AddNamedPipe(options => options.ThreadCount = 10)
+                    .AddService<IProxy, Proxy>();
+            });
+        }
+    }
+}
